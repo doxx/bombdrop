49dacc346021781feb40070c4cd4332c4d65f3ff
doxx <blyon@blyon.com>
2025-09-22 11:36:40 -0400

    Restore working bombdrop.go from commit 7c16e6b without raw spoofing complexity

--- Apple/cpu_mon.sh
+++
@@ -1,1 +0,0 @@
-while true; do echo -n "$(date '+%Y-%m-%d %H:%M:%S'),"; top -l 4 | egrep "mDNSResponder|kernel_task" | grep -v Hel | tail -2 | awk '{print $3}' | tr '\n' ',' | sed 's/,$/\n/'; sleep 1; done
--- Apple/dns_mon.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/csh
-while true; do echo -n "$(date '+%Y-%m-%d %H:%M:%S'),"; start=$(gdate +%s%N); if timeout 3 dscacheutil -q host -a name www.google.com >/dev/null; then echo $(($(gdate +%s%N) - start)) | awk '{printf "%.1f\n", $1/1000000}'; else echo ""; fi; sleep 4; done 
